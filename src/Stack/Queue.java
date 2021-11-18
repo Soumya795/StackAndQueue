aadfda9f75dbb4a3d4763fc484b385cf1194664c
Soumya795 <soumyasubhakanta97@gmail.com>
2021-11-18 15:21:08 +0530

    Added: Ability to DeQueue

--- src/Stack/Queue.java
+++ src/Stack/Queue.java
@@ -4,9 +4,6 @@ public class Queue {
     MyNode front;
     MyNode rear;
 
-    /*
-     * This Method is used to add elements to Queue
-     */
     public void enqueue(int element) {
         MyNode node = new MyNode();
         node.key = element;
@@ -17,6 +14,17 @@ public class Queue {
             rear.next = node;
             rear = node;
         }
+    }
+    /* This method is used to delete elements from the queue */
+    public void dequeue() {
+        if (front == null)
+            System.out.println("Queue Underflow");
+        else {
+            MyNode temp = front;
+            System.out.println("Dequeued element is " + temp.key);
+            front = front.next;
+        }
+
     }
     /* This Method is used to display the elements in Queue. */
     public void display() {
@@ -32,11 +40,13 @@ public class Queue {
         }
     }
 
+
     public static void main(String[] args) {
         Queue queue = new Queue();
         queue.enqueue(56);
         queue.enqueue(30);
         queue.enqueue(70);
+        queue.dequeue();
         queue.display();
 
     }
