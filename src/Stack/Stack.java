f408c0fbd7f315a2e6c5047eba81e53bb1315893
Soumya795 <soumyasubhakanta97@gmail.com>
2021-11-18 15:15:32 +0530

    Added: Ability To Peak And Pop

--- src/Stack/Stack.java
+++ src/Stack/Stack.java
@@ -6,15 +6,29 @@ class MyNode {
 public class Stack {
     private MyNode top;
 
-    /*
-     * This method is used to add elements to stack
-     * @param element: elements to be added.
-     */
     public void push(int element) {
         MyNode node = new MyNode();
         node.key = element;
         node.next = top;
         top = node;
+    }
+
+    /*  This Method is used to return top element from the stack */
+    public int peak() {
+        System.out.println("Top element is " +top.key);
+        return top.key;
+    }
+
+    /* This method is used to delete element from the stack */
+    public void pop() {
+        if (top == null)
+            System.out.println("Stack empty");
+        else {
+            MyNode temp = top;
+            System.out.println("Popped out element is " + temp.key);
+            top = top.next;
+        }
+
     }
     /* This method is used to display the stack list */
     public void display() {
@@ -29,14 +43,24 @@ public class Stack {
             }
         }
     }
+        public static void main(String[] args) {
+            Stack stack = new Stack();
+            stack.push(70);
+            stack.push(30);
+            stack.push(56);
+            stack.display();
+            //To perform peak and pop till the list is empty
+            stack.peak();
+            stack.pop();
+            stack.display();
+            stack.peak();
+            stack.pop();
+            stack.display();
+            stack.peak();
+            stack.pop();
+            stack.display();
 
-    public static void main(String[] args) {
-        Stack stack = new Stack();
-        stack.push(70);
-        stack.push(30);
-        stack.push(56);
-        stack.display();
-
+        }
     }
-}
+
 
