70a0116cbe9131d7fccb87c99d8a1275b394c491
Soumya795 <soumyasubhakanta97@gmail.com>
2021-11-18 15:19:01 +0530

    Added: Create A Queue

---
+++ src/Stack/Queue.java
@@ -0,0 +1,43 @@
+package Stack;
+
+public class Queue {
+    MyNode front;
+    MyNode rear;
+
+    /*
+     * This Method is used to add elements to Queue
+     */
+    public void enqueue(int element) {
+        MyNode node = new MyNode();
+        node.key = element;
+        if (front == null) {
+            front = node;
+            rear = node;
+        } else {
+            rear.next = node;
+            rear = node;
+        }
+    }
+    /* This Method is used to display the elements in Queue. */
+    public void display() {
+        if (front == null)
+            System.out.println("Queue is empty");
+        else {
+            MyNode temp = front;
+            System.out.println("Queue list is -");
+            while (temp != null) {
+                System.out.println(temp.key);
+                temp = temp.next;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Queue queue = new Queue();
+        queue.enqueue(56);
+        queue.enqueue(30);
+        queue.enqueue(70);
+        queue.display();
+
+    }
+}
